babfe47b7eca5adb0ab057953232cf5cee13e468
Aaron Mendes Silva <95545163+Aaronfrombr@users.noreply.github.com>
2024-03-11 21:23:06 -0400

    Add files via upload

--- myAccount.js
+++ myAccount.js
@@ -18,7 +18,7 @@ function run(username, funcaoMenuPrincipal) {
     voltarMenuPrincipal = funcaoMenuPrincipal;
     dashboard();
   } else {
-    throw new Exception("Usuário não existe!");
+    throw new Error("Usuário não existe!"); // Usar `Error` em vez de `Exception`
   }
 }
 
@@ -53,23 +53,108 @@ function dashboard() {
 
 function consultaSaldo() {
   console.log("### Consultando saldo ###");
-  //Programe a operação de consulta de saldo aqui
-  /*Não esqueça de invocar a função dashboard() após a execução
-  da operação para o usuário poder continuar operando sua conta; */
+
+  if (!caminhoUsuario) {
+    console.log(chalk.bgRed.black("Caminho do arquivo não especificado."));
+    dashboard(); // Retornar ao dashboard após o erro
+    return;
+  }
+
+  if (!fs.existsSync(caminhoUsuario)) {
+    console.log(chalk.bgRed.black("Arquivo de conta não encontrado."));
+    dashboard(); // Retornar ao dashboard após o erro
+    return;
+  }
+
+  try {
+    const content = fs.readFileSync(caminhoUsuario, "utf8");
+    const conteudoSaldo = JSON.parse(content);
+    const dinheiro = conteudoSaldo.monetario;
+
+    console.log(chalk.bgGreen.white(`Você tem um total de ${dinheiro}R$`));
+
+    dashboard(); // Chamar dashboard após a consulta
+  } catch (err) {
+    console.log(chalk.bgRed.black("Erro ao consultar saldo:", err));
+    dashboard(); // Retornar ao dashboard após o erro
+  }
 }
 
 function deposita() {
   console.log("### Depositando ###");
-  //Programe a operação de depósito aqui
-  /*Não esqueça de invocar a função dashboard() após a execução
-  da operação para o usuário poder continuar operando sua conta; */
+  inquirer
+    .prompt([
+      {
+        type: "number",
+        name: "deposito",
+        message:
+          "Informe o valor a ser depositado (somente números):",
+      },
+    ])
+    .then((respostas) => {
+      const deposito = respostas["deposito"];
+
+      if (!deposito) {
+        console.log(chalk.bgRed.black("Informe valor !"));
+        return;
+      } else if (deposito === 0) { // Usar `===` em vez de `==`
+        console.log(chalk.bgRed.black("Deposito em Branco!"));
+        return;
+      }
+
+      const caminho = path.join("contas", `${usuario}.json`);
+      const conteudo = fs.readFileSync(caminho, "utf8");
+      const dadosConta = JSON.parse(conteudo);
+
+      dadosConta.monetario += deposito;
+
+      fs.writeFileSync(caminho, JSON.stringify(dadosConta));
+
+      console.log(chalk.bgGreen.white("Depósito realizado!"));
+
+      dashboard();
+    })
+    .catch((err) => console.log(err));
 }
 
 function saca() {
   console.log("### Sacando ###");
-  //Programe a operação de saque aqui
-  /*Não esqueça de invocar a função dashboard() após a execução
-  da operação para o usuário poder continuar operando sua conta; */
+
+  inquirer
+    .prompt([
+      {
+        type: "number",
+        name: "saque",
+        message:
+          "Informe o valor a ser sacado (somente números):",
+      },
+    ])
+    .then((respostas) => {
+      const saque = respostas["saque"];
+
+      if (!saque) {
+        console.log(chalk.bgRed.black("Informe valor!"));
+        return;
+      }
+
+      const caminho = path.join("contas", `${usuario}.json`);
+      const conteudo = fs.readFileSync(caminho, "utf8");
+      const dadosConta = JSON.parse(conteudo);
+
+      if (saque > dadosConta.monetario) {
+        console.log(chalk.bgRed.black("O saque é maior que o disponivel."));
+        return;
+      }
+
+      dadosConta.monetario -= saque;
+
+      fs.writeFileSync(caminho, JSON.stringify(dadosConta));
+
+      console.log(chalk.bgGreen.white("Saque realizado!"));
+
+      dashboard();
+    })
+    .catch((err) => console.log(err));
 }
 
 module.exports = run;
